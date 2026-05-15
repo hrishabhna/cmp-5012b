c7d62e63966b83758177cf9a73cdbb2eecfe3759
Able <jau23gbu@uea.ac.uk>
2026-05-15 11:37:26 +0100

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-const data    = require('../data/chartData');
+const data    = require('../data/chartData'); // this imports the data from chartData.js
  
-router.get('/weight',   (req, res) => res.json(data.weight()));
-router.get('/calories', (req, res) => res.json(data.calories()));
-router.get('/exercise', (req, res) => res.json(data.exercise()));
+router.get('/weight',   (req, res) => res.json(data.weight()));// this returns the weight data 
+router.get('/calories', (req, res) => res.json(data.calories())); // this returns the calories data
+router.get('/exercise', (req, res) => res.json(data.exercise())); //this returns the excercise data 
  
-module.exports = router;
+module.exports = router;//exports the data into app.js 
