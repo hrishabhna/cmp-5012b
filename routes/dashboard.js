719ab55626834aba89629eeec794551a32dada7c
hrishabhna <qcv24xfu@uea.ac.uk>
2026-05-15 14:51:03 +0100

    formatting changes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 const data    = require('../data/chartData');
- 
+
 router.get('/weight',   (req, res) => res.json(data.weight()));
 router.get('/calories', (req, res) => res.json(data.calories()));
 router.get('/exercise', (req, res) => res.json(data.exercise()));
- 
+
 module.exports = router;
